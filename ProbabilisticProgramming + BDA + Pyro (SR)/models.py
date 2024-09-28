af797a0a448f28ce46da838922ea94ba8f037a5e
TheCuriousCurator <curious.curator.services@gmail.com>
2024-09-28 17:43:05 +0530

    add models.py to pyro

---
+++ ProbabilisticProgramming + BDA + Pyro (SR)/models.py
@@ -0,0 +1,42 @@
+import tqdm
+import torch
+tt = torch.tensor
+import pyro
+from pyro.infer import SVI, Trace_ELBO
+import pyro.infer.autoguide
+from pyro.infer.autoguide import AutoMultivariateNormal, AutoDiagonalNormal, init_to_mean,  AutoLaplaceApproximation
+from pyro.optim import Adam
+from pyro.distributions import Normal, Exponential
+from pyro.infer.mcmc import NUTS, MCMC
+from pyro.infer.mcmc.nuts import HMC
+
+class RegressionBase:
+    def __init__(self, df, categoricals=None):
+        if categoricals is None:
+            categoricals = []
+        for col in set(df.columns) - set(categoricals):
+            setattr(self, col, tt(df[col].values).double())
+        for col in categoricals:
+            setattr(self, col, tt(df[col].values).long())
+            
+    def __call__(self):
+        raise NotImplementedError
+        
+    def train(self, num_steps, lr=1e-2, restart=True, autoguide=None, use_tqdm=True):
+        if restart:
+            pyro.clear_param_store()
+            if autoguide is None:
+                autoguide = AutoMultivariateNormal
+            else:
+                autoguide = getattr(pyro.infer.autoguide, autoguide)
+            self.guide = autoguide(self, init_loc_fn=init_to_mean)
+        svi = SVI(self, guide=self.guide, optim=Adam({"lr": lr}), loss=Trace_ELBO())
+        loss = []
+        if use_tqdm:
+            iterator = tqdm.notebook.tnrange(num_steps)
+        else:
+            iterator = range(num_steps)
+        for _ in iterator:
+            loss.append(svi.step())
+        return loss
+
