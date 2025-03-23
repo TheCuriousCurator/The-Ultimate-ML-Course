307180baeeb5d47504c759ae4734b168088d4d20
TheCuriousCurator <curious.curator.services@gmail.com>
2025-03-23 13:15:52 +0530

    added git_tutorial.py

---
+++ Git/git_tutorial.py
@@ -0,0 +1,11 @@
+############ Feature request ############ 
+# Idea1(fuctionality): support abitararily many inputs
+# Idea2(performance): increase performance
+# Idea3(documentation): improve documentation
+# Idea4(security): security features
+############ -----END------- ############ 
+
+# master
+def f(a, b):
+    return (a + b)
+
