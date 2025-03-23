0552ba42d865a2030d59e1b490fac02e689cc9e4
TheCuriousCurator <curious.curator.services@gmail.com>
2025-03-23 13:31:04 +0530

    using sum to improve perfomance

--- Git/git_tutorial.py
+++ Git/git_tutorial.py
@@ -1,11 +1,3 @@
-############ Feature request ############ 
-# Idea1(fuctionality): support abitararily many inputs
-# Idea2(performance): increase performance
-# Idea3(documentation): improve documentation
-# Idea4(security): security features
-############ -----END------- ############ 
-
-# master
 def f(a, b):
-    return (a + b)
+    return sum(a, b)
 
